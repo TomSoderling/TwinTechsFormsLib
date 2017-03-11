efbc186d329d0799cf28675880420cde49f56673
Tom Soderling <hey_tommy@icloud.com>
2017-03-11 16:45:29 -0600

    formatting

--- TwinTechsForms/TwinTechsFormsExample/TwinTechs/Example/SampleMenu.xaml.cs
+++ TwinTechsForms/TwinTechsFormsExample/TwinTechs/Example/SampleMenu.xaml.cs
@@ -12,41 +12,41 @@ namespace TwinTechs.Example
 {
 	public partial class SampleMenu : ContentPage
 	{
-		public SampleMenu ()
+		public SampleMenu()
 		{
-			InitializeComponent ();
+			InitializeComponent();
 		}
 
 		#region fast cell samples
 
-		void OnDefaultSimpleCell (object sender, object args)
+		void OnDefaultSimpleCell(object sender, object args)
 		{
-			Navigation.PushAsync (new ViewCellSimple ());
+			Navigation.PushAsync(new ViewCellSimple());
 		}
 
-		void OnFastSimpleCell (object sender, object args)
+		void OnFastSimpleCell(object sender, object args)
 		{
-			Navigation.PushAsync (new FastCellSamplePage ());
+			Navigation.PushAsync(new FastCellSamplePage());
 		}
 
-		void OnFastSimpleCellLots (object sender, object args)
+		void OnFastSimpleCellLots(object sender, object args)
 		{
-			Navigation.PushAsync (new FastSimpleCellLots ());
+			Navigation.PushAsync(new FastSimpleCellLots());
 		}
 
-		void OnFastCellUnevenHeights (object sender, object args)
+		void OnFastCellUnevenHeights(object sender, object args)
 		{
-			Navigation.PushAsync (new FastSimpleCellUnevenHeight ());
+			Navigation.PushAsync(new FastSimpleCellUnevenHeight());
 		}
 
-		void OnDefaultComplexCell (object sender, object args)
+		void OnDefaultComplexCell(object sender, object args)
 		{
-			Navigation.PushAsync (new ViewCellComplexSamplePage ());
+			Navigation.PushAsync(new ViewCellComplexSamplePage());
 		}
 
-		void OnFastComplexCell (object sender, object args)
+		void OnFastComplexCell(object sender, object args)
 		{
-			Navigation.PushAsync (new FastCellComplexSamplePage ());
+			Navigation.PushAsync(new FastCellComplexSamplePage());
 		}
 
 		#endregion
@@ -54,121 +54,107 @@ namespace TwinTechs.Example
 		#region Xlabs GridView improvements
 
 
-		void OnGridViewPerformance (object sender, object args)
+		void OnGridViewPerformance(object sender, object args)
 		{
-			Navigation.PushAsync (new GridViewPerformance ());
+			Navigation.PushAsync(new GridViewPerformance());
 		}
 
-		void OnGridViewPerformance2 (object sender, object args)
+		void OnGridViewPerformance2(object sender, object args)
 		{
-			Navigation.PushAsync (new GridViewPerformance2 ());
+			Navigation.PushAsync(new GridViewPerformance2());
 		}
 
-		void OnGridViewPerformance3 (object sender, object args)
+		void OnGridViewPerformance3(object sender, object args)
 		{
-			Navigation.PushAsync (new GridViewXamlPerformance ());
+			Navigation.PushAsync(new GridViewXamlPerformance());
 		}
 
-		void OnGridViewProperties (object sender, object args)
+		void OnGridViewProperties(object sender, object args)
 		{
-			Navigation.PushAsync (new GridViewOptions ());
+			Navigation.PushAsync(new GridViewOptions());
 		}
 
 		#endregion
 
 		#region gestures
 
-		void OnTapGesture (object sender, object args)
+		void OnTapGesture(object sender, object args)
 		{
-			Navigation.PushAsync (new TapGestureExample ());
+			Navigation.PushAsync(new TapGestureExample());
 		}
 
 
-		void OnPageScrollerExample (object sender, object args)
+		void OnPageScrollerExample(object sender, object args)
 		{
-			Navigation.PushAsync (new ViewScrollerExample ());
-
+			Navigation.PushAsync(new ViewScrollerExample());
 		}
 
-		void OnSwipeGesture (object sender, object args)
+		void OnSwipeGesture(object sender, object args)
 		{
-			Navigation.PushAsync (new SwipeGestureExample ());
-
+			Navigation.PushAsync(new SwipeGestureExample());
 		}
 
-		void OnLongPressGesture (object sender, object args)
+		void OnLongPressGesture(object sender, object args)
 		{
-			Navigation.PushAsync (new LongPressGestureExample ());
-
+			Navigation.PushAsync(new LongPressGestureExample());
 		}
 
-		void OnPanGesture (object sender, object args)
+		void OnPanGesture(object sender, object args)
 		{
-			Navigation.PushAsync (new PanGestureExample ());
-
+			Navigation.PushAsync(new PanGestureExample());
 		}
 
-		void OnPanGestureAlternate (object sender, object args)
+		void OnPanGestureAlternate(object sender, object args)
 		{
-			Navigation.PushAsync (new PanGestureAlternateRouting ());
-
+			Navigation.PushAsync(new PanGestureAlternateRouting());
 		}
 
-		void OnPanGestureAlternateDelegate (object sender, object args)
+		void OnPanGestureAlternateDelegate(object sender, object args)
 		{
-			Navigation.PushAsync (new PanGestureAlternateRoutingDelayAndDelegate ());
-
+			Navigation.PushAsync(new PanGestureAlternateRoutingDelayAndDelegate());
 		}
 
-		void OnPan2Gesture (object sender, object args)
+		void OnPan2Gesture(object sender, object args)
 		{
-			Navigation.PushAsync (new PanGestureExample2 ());
-
+			Navigation.PushAsync(new PanGestureExample2());
 		}
 
 
-		void OnPan3Gesture (object sender, object args)
+		void OnPan3Gesture(object sender, object args)
 		{
-			Navigation.PushAsync (new PanGestureExample3 ());
-
+			Navigation.PushAsync(new PanGestureExample3());
 		}
 
-		void OnPinchGesture (object sender, object args)
+		void OnPinchGesture(object sender, object args)
 		{
-			Navigation.PushAsync (new GridViewOptions ());
-
+			Navigation.PushAsync(new GridViewOptions());
 		}
 
-		void OnMixedGestures (object sender, object args)
+		void OnMixedGestures(object sender, object args)
 		{
-			Navigation.PushAsync (new MultiGestureExample ());
-
+			Navigation.PushAsync(new MultiGestureExample());
 		}
 
 
-		void OnGesturesInChildViews (object sender, object args)
+		void OnGesturesInChildViews(object sender, object args)
 		{
-			Navigation.PushAsync (new GesturesInChildViewsExample ());
-
+			Navigation.PushAsync(new GesturesInChildViewsExample());
 		}
 
-		void OnCellGestures (object sender, object args)
+		void OnCellGestures(object sender, object args)
 		{
-			Navigation.PushAsync (new SwipeyListExample ());
-
+			Navigation.PushAsync(new SwipeyListExample());
 		}
 
 
-		void OnYoutubeStyleGestures (object sender, object args)
+		void OnYoutubeStyleGestures(object sender, object args)
 		{
-			Navigation.PushAsync (new GestureYoutubeLikeExample ());
-
+			Navigation.PushAsync(new GestureYoutubeLikeExample());
 		}
 
-		void OnDragListGestures (object sender, object args)
+		void OnDragListGestures(object sender, object args)
 		{
-			Navigation.PushAsync (new GestureScrollListExample ());
-
+			Navigation.PushAsync(new GestureScrollListExample());
 		}
 
 
@@ -177,37 +163,34 @@ namespace TwinTechs.Example
 
 		#region Page in page
 
-		void OnPageInPageSimple (object sender, object args)
+		void OnPageInPageSimple(object sender, object args)
 		{
-			var pageInPage = new PageInPageSample ();
-			Navigation.PushAsync (pageInPage);
-			
+			var pageInPage = new PageInPageSample();
+			Navigation.PushAsync(pageInPage);
 		}
 
-		void OnEmbeddedNavigationPage (object sender, object args)
+		void OnEmbeddedNavigationPage(object sender, object args)
 		{
-			var navigationPageInPage = new NavigationPageInPage ();
-			Navigation.PushAsync (navigationPageInPage);
-			
+			var navigationPageInPage = new NavigationPageInPage();
+			Navigation.PushAsync(navigationPageInPage);
 		}
 
 		#endregion
 
 		#region composition examples
 
-		void OnCircleView (object sender, object args)
+		void OnCircleView(object sender, object args)
 		{
-			Navigation.PushAsync (new CircleViewSamplePage ());
-			
+			Navigation.PushAsync(new CircleViewSamplePage());
 		}
 
 		#endregion
 
 		#region SvgImage examples
 
-		async void OnSvgImage (object sender, EventArgs args)
+		async void OnSvgImage(object sender, EventArgs args)
 		{
-			await Navigation.PushAsync (new SvgImageSample.SvgImageSamplePage ());
+			await Navigation.PushAsync(new SvgImageSample.SvgImageSamplePage());
 		}
 
 		#endregion
